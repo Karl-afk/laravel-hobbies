187121f4dc23ca8c1fe4a7732ae64b54f5ec1941
Karl Weinmueller <karlweinmueller@me.com>
2022-10-05 12:35:56 +0200

    added Font Awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'MyHobbies') }}</title>
+    <title>@yield('title', 'MyHobbies')</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
